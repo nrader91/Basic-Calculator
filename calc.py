649fe368199264817f45281e7be73e17fc8fe6a5
nrad <nicholas.rader91@outlook.com>
2021-11-27 11:52:57 -0700

    added float numbers & functionality

---
+++ calc.py
@@ -0,0 +1,83 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "e1ad5ff1-1494-4303-9ee2-e32b3ad2ed19",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Test\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"Test\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "a17b6b1b-bcef-48bf-a46d-8d2625da949f",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "Enter a number:  5\n",
+      "Enter another number:  2\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "52\n"
+     ]
+    }
+   ],
+   "source": [
+    "num1 = input(\"Enter a number: \")\n",
+    "\n",
+    "num2 = input(\"Enter another number: \")\n",
+    "\n",
+    "result = (num1 + num2)\n",
+    "\n",
+    "print(result)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "e8044880-5d1d-4ce6-86cc-f7733aee9cf7",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
